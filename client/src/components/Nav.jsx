38ab557d5088d162ca21fc0ad031399e2870810b
jngong <34215359+jngong@users.noreply.github.com>
2020-07-15 20:52:53 -0400

    added logo and styled components

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React from "react";
+import logo from "../assets/neptune-logo.png";
+import styled from "styled-components";
 
+const NavBar = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+`
+
+const Logo = styled.img`
+  width: 50px;
+  height: 50px;
+  margin: 1rem;
+`
 export const Nav = () => {
   return (
-    <nav>
-      <div>Logo</div>
+    <NavBar>
+      <Logo src={logo} />
       <button>Add A Project</button>
-    </nav>
-  )
-}
+    </NavBar>
+  );
+};
